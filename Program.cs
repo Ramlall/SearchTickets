5b60801ad746d930baf88bef3633c7f60dc3eeaa
Tariq <tariq@smunlisted.com>
2015-11-08 18:19:30 -0500

    Initial commit. Program has simple function but works.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchTickets
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string websiteName = "http://www.ramesar.net/"; // Put the website name here. I think it has to have the http://
+            string webData = wc.DownloadString(websiteName);
+
+            // Writes the website data to the console.
+            Console.WriteLine(webData);
+            }
+        }
+    }
---
+++ readme.md
@@ -0,0 +1,1 @@
+A C# program to look up the price of a ticket on a website and return the result.
